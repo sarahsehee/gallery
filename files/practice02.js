e79294993934b8b2e8cd7add5091b3a03f7622f5
commit <>
2018-07-17 17:25:29 +0900

    jam

--- files/practice02.js
+++ files/practice02.js
@@ -1,6 +1,6 @@
 ////버튼 호버시, "어서 눌러주세요" 라는 멘트가 버튼 밑에 뜬다.
 /////버튼을 누르면, 계속 숫자가 바뀐다.
-///숫자 중에 "34"가 나오면 "당첨되셨습니다." 라는 멘트의 팝업이 뜬다. (from 예지)
+/// (from 예지)
 
 // get the random number
 var num = [1, "히히", 34, 56, 787, 84, 85, 345, 66, 23];
@@ -12,4 +12,10 @@ function lotto() {
   var randomPick = num[Math.floor(Math.random() * num.length)];
 
   document.getElementById("result").innerHTML = randomPick;
+
+  if (randomPick === "히히") {
+    modal.style.display = "block";
+  } else if (randomPick !== "히히") {
+    modal.style.display = "none";
+  }
 }
