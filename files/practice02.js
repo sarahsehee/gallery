b743961d68924e3bafa9fefb22ef153094870440
commit <>
2018-07-17 17:05:43 +0900

    second

--- files/practice02.js
+++ files/practice02.js
@@ -1,30 +1,15 @@
-///////////
-var color = ["white", "yellow", "blue", "aqua", "purple", "black", "orange"];
+////버튼 호버시, "어서 눌러주세요" 라는 멘트가 버튼 밑에 뜬다.
+/////버튼을 누르면, 계속 숫자가 바뀐다.
+///숫자 중에 "34"가 나오면 "당첨되셨습니다." 라는 멘트의 팝업이 뜬다. (from 예지)
 
-var i = 0;
-function changeColor() {
-  i++;
-  if (i >= color.length) {
-    i = 0;
-  }
+// get the random number
+var num = [1, "히히", 34, 56, 787, 84, 85, 345, 66, 23];
 
-  var bodyTag = document.getElementById("galleryZone");
-  galleryZone.style.backgroundColor = color[i];
-}
+// Get the modal
+var modal = document.getElementById("myModal");
 
-//////////////
+function lotto() {
+  var randomPick = num[Math.floor(Math.random() * num.length)];
 
-var num = 1;
-
-function gallery(direct) {
-  if (direct) {
-    if (num == 7) return;
-    num++;
-  } else {
-    if (num == 1) return;
-    num--;
-  }
-
-  var imgTag = document.getElementById("photo");
-  imgTag.setAttribute("src", "../imgs/pic_" + num + ".jpg");
+  document.getElementById("result").innerHTML = randomPick;
 }
